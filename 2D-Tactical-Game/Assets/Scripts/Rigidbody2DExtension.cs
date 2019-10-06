dad5cf7a9e5e823fe1ef3bb1ef67b870ec74dce4
trevinofernando <trevino.96@hotmail.com>
2019-10-06 15:07:03 -0400

    added explosion/ impact forces to guns, fixed double jumping, fixed death animation bug where the collider didn't match the position of the sprites

---
+++ 2D-Tactical-Game/Assets/Scripts/Rigidbody2DExtension.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Rigidbody2DExtension
+{
+    public static void AddExplosionForce(this Rigidbody2D body, Vector3 explosionPosition, float explosionRadius, float explosionForce)
+    {
+        var dir = (body.transform.position - explosionPosition);
+        float wearoff = 1 - (dir.magnitude / explosionRadius);
+        body.AddForce(dir.normalized * explosionForce * wearoff);
+        //body.AddForce(new Vector2( (dir.normalized.x * 10f), dir.normalized.y)* explosionForce * wearoff);
+    }
+
+    public static void AddExplosionForce(this Rigidbody2D body, Vector3 explosionPosition, float explosionRadius, float explosionForce, float sideForce)
+    {
+        var dir = (body.transform.position - explosionPosition);
+        float wearoff = 1 - (dir.magnitude / explosionRadius);
+        Vector3 baseForce = dir.normalized * explosionForce * wearoff;
+        //body.AddForce(baseForce);
+
+        float sideWearoff = 1 - (sideForce / explosionRadius);
+        Vector3 extraForce = Vector2.right * dir.normalized.x * explosionForce * sideWearoff;
+        body.AddForce(baseForce + extraForce);
+    }
+}
