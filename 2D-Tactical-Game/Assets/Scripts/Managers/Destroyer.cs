048315a1284b1e26d2d90d43e99404c81a13e26f
trevinofernando <trevino.96@hotmail.com>
2019-10-11 15:33:36 -0400

    Added 2 explosions edited in GIMP with their animation and prefab. Note: all the deleting and adding of files is because I reorginized some folders.

---
+++ 2D-Tactical-Game/Assets/Scripts/Managers/Destroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public float timeToAutoDestroy = 1f;
+    
+    void Start()
+    {
+        Destroy(gameObject, timeToAutoDestroy);
+    }
+}
