57645c58fedd15ac38b1fd146e127372a03d847f
alexogilbee <aogilbee0699@gmail.com>
2019-10-21 02:22:30 -0400

    added semi-functional pause menu

--- 2D-Tactical-Game/Assets/Scripts/Menus&UI/MainMenu.cs
+++ 2D-Tactical-Game/Assets/Scripts/Menus&UI/MainMenu.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 public class MainMenu : MonoBehaviour
 {
     public void QuitGame ()
---
+++ 2D-Tactical-Game/Assets/Scripts/Menus&UI/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Main Menu Button
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // Quit Game Button
+    public void QuitGame()
+    {
+        Debug.Log("the game has quit.");
+        Application.Quit();
+    }
+}
