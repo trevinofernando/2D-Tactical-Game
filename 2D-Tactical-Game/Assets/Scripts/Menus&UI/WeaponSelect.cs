3bd74b39ccd335e0b8e77179d46475454d8f8d92
alexogilbee <aogilbee0699@gmail.com>
2019-10-28 15:41:59 -0400

    weapon menu / potential merge conflict

---
+++ 2D-Tactical-Game/Assets/Scripts/Menus&UI/WeaponSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelect : MonoBehaviour
+{
+    public GameManager gm;
+    public int weaponIndex;
+    public GameObject weaponMenu;
+
+    private GameObject currPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // update this sprite (maybe do in global unsire!)
+
+        // figure out status of menu
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this doesnt work here because the obj is disabled when you turn it off lol
+        
+        
+    }
+
+    public void CallWeaponSwap()
+    {
+        // call ChangeWeapon
+        currPlayer = gm.teams[gm.currTeamTurn, gm.currSoldierTurn[gm.currTeamTurn]];  // find the current player
+        currPlayer.GetComponent<WeaponController>().ChangeWeapon(weaponIndex);        // change weapon to selected button
+    }
+
+    public void OnEnable()
+    {
+        currPlayer = gm.teams[gm.currTeamTurn, gm.currSoldierTurn[gm.currTeamTurn]];  // find the current player
+        currPlayer.GetComponent<WeaponController>().ChangeWeapon(0);        // change weapon to selected button
+    }
+}
