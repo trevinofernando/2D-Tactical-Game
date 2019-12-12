65ea218b9195b7cb2126de1955745b247ed37727
trevinofernando <trevino.96@hotmail.com>
2019-12-12 17:59:42 -0500

    improved visual cues in arsenal for weapons with 0 ammo

--- 2D-Tactical-Game/Assets/Scripts/Menus&UI/WeaponSelect.cs
+++ 2D-Tactical-Game/Assets/Scripts/Menus&UI/WeaponSelect.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class WeaponSelect : MonoBehaviour
@@ -9,8 +10,11 @@ public class WeaponSelect : MonoBehaviour
     public WeaponCodes wc;
     public GameObject weaponMenu;
     public TextMeshProUGUI ammo;
+    public Button btn;
 
     private GameObject currPlayer;
+    private Color red = new Color32(150,0,0,255);
+    private Color black = new Color32(0,0,0,255);
 
     public void CallWeaponSwap()
     {
@@ -25,7 +29,18 @@ public class WeaponSelect : MonoBehaviour
             gm.isArsenalOpen = true;
             if(!GlobalVariables.Instance.isTeamAI[gm.currTeamTurn]){
                 currPlayer = gm.teams[gm.currTeamTurn, gm.currSoldierTurn[gm.currTeamTurn]];  // find the current player
-                ammo.SetText(currPlayer.GetComponent<PlayerSettings>().AmmoCount((int)wc).ToString("00"));
+                
+                int ammoCount = currPlayer.GetComponent<PlayerSettings>().AmmoCount((int)wc);
+                ammo.SetText(ammoCount.ToString("00"));
+                
+                var color = btn.colors;
+                if(ammoCount <= 0){
+                    color.normalColor = red;
+                }else{
+                    color.normalColor = black;
+                }
+                btn.colors = color;
+
                 currPlayer.GetComponent<WeaponController>().ChangeWeapon((int)WeaponCodes.Gauntlet);        // change weapon to gauntlet
             }
         }else{
