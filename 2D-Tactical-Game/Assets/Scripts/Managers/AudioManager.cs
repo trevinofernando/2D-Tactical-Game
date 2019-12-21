074365805001c35950510b423042862a476a25ee
trevinofernando <trevino.96@hotmail.com>
2019-12-21 12:43:13 -0500

    Fixed Audio leaking between scenes

--- 2D-Tactical-Game/Assets/Scripts/Managers/AudioManager.cs
+++ 2D-Tactical-Game/Assets/Scripts/Managers/AudioManager.cs
@@ -20,7 +20,7 @@ public class AudioManager : MonoBehaviour
             return;
         }
 
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
 
         foreach(Sound s in sounds)
         {
